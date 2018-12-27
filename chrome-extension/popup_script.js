22063317168bd59afea8d82fe1647f4084027d07
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2018-12-28 00:50:53 +0900

    refactor: キー入力無効化script file をフォルダに集約
    構成をわかりやすくするため
    無効化scriptを scripts フォルダに移動

--- chrome-extension/popup_script.js
+++ chrome-extension/popup_script.js
@@ -4,18 +4,18 @@ document.getElementById('open').addEventListener('click', () => {
 		currentWindow: true,
 	}, ([tab]) => {
 		chrome.tabs.executeScript(tab.id, {
-			file: 'disable-ctrl_w.js',
+			file: 'scripts/disable-ctrl_w.js',
 		});
 		const disableReloadShortcutInput = document.getElementById('disable-ctrl_r').checked;
 		if (disableReloadShortcutInput) {
 			chrome.tabs.executeScript(tab.id, {
-				file: 'disable-ctrl_r.js',
+				file: 'scripts/disable-ctrl_r.js',
 			});
 		}
 		const disableAllKeyInput = document.getElementById('disable-all').checked;
 		if (disableAllKeyInput) {
 			chrome.tabs.executeScript(tab.id, {
-				file: 'disable-all.js',
+				file: 'scripts/disable-all.js',
 			});
 		}
 		chrome.windows.create({
