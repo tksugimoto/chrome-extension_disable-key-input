1a54d00d4e5ac64e7b3b967b805910ed7f12ccb5
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2024-09-15 19:24:39 +0900

    feat: migrate to manifest v3

--- chrome-extension/popup_script.js
+++ chrome-extension/popup_script.js
@@ -3,22 +3,31 @@ document.getElementById('open').addEventListener('click', () => {
 		active: true,
 		currentWindow: true,
 	}, ([tab]) => {
-		chrome.tabs.executeScript(tab.id, {
-			allFrames: true,
-			file: 'scripts/disable-ctrl_w.js',
+		chrome.scripting.executeScript({
+			target: {
+				tabId: tab.id,
+				allFrames: true,
+			},
+			files: ['scripts/disable-ctrl_w.js'],
 		});
 		const disableReloadShortcutInput = document.getElementById('disable-ctrl_r').checked;
 		if (disableReloadShortcutInput) {
-			chrome.tabs.executeScript(tab.id, {
-				allFrames: true,
-				file: 'scripts/disable-ctrl_r.js',
+			chrome.scripting.executeScript({
+				target: {
+					tabId: tab.id,
+					allFrames: true,
+				},
+				files: ['scripts/disable-ctrl_r.js'],
 			});
 		}
 		const disableAllKeyInput = document.getElementById('disable-all').checked;
 		if (disableAllKeyInput) {
-			chrome.tabs.executeScript(tab.id, {
-				allFrames: true,
-				file: 'scripts/disable-all.js',
+			chrome.scripting.executeScript({
+				target: {
+					tabId: tab.id,
+					allFrames: true,
+				},
+				files: ['scripts/disable-all.js'],
 			});
 		}
 		chrome.windows.create({
